10f42932be5d95edf4621a1026b877db2052cd36
konhi <hello.konhi@gmail.com>
2021-09-12 16:06:39 +0200

    feat: save news to file

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,8 @@
 import { getArticles } from './scraper'
+import fs from 'fs';
 
-getArticles().then((articles => console.log(articles)))
+const PATHS = {
+  news: 'data/news.json',
+}
+
+getArticles().then(articles => fs.writeFileSync(PATHS.news, JSON.stringify(articles, null, 2))
