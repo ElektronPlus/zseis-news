39b7bcdd5f74596a9550c238c3e7ae01548ff0b5
konhi <hello.konhi@gmail.com>
2021-09-16 19:02:56 +0200

    style: then instead of async/await, remove imports

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,6 @@
 import getNewsList from './scraper'
-import fs from 'fs'
 import sendNewsByWebhook from './discord'
-import { options, PATHS } from './options'
+import { options } from './options'
 
-async function main () {
-  const newsList = await getNewsList()
-  // const file = fs.readFileSync(PATHS.news, 'utf8')
-
-  // const json = JSON.stringify(newsList, null, 2)
-
-  sendNewsByWebhook(newsList, options.url)
-
-  // fs.writeFileSync(PATHS.news, json)
-}
-
-main()
+getNewsList()
+  .then(newsList => sendNewsByWebhook(newsList, options.url))
