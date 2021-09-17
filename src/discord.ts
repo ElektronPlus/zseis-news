61cc2b74f89039fb81bde8a50fb653578ebdfe97
konhi <hello.konhi@gmail.com>
2021-09-17 16:08:24 +0200

    feat: fallback for token

--- src/discord.ts
+++ src/discord.ts
@@ -5,7 +5,6 @@ import options from './options'
 import { writeObjectToFile } from './utils'
 
 const sentToDiscord = JSON.parse(fs.readFileSync(options.PATHS.sentToDiscord, 'utf8'))
-
 async function wasSentBefore(news: News): Promise<boolean> {
   return (sentToDiscord.md5.includes(news.md5))
 }
@@ -15,12 +14,12 @@ async function setAsSent(news: News): Promise<void> {
 }
 
 export default async function sendNewsByWebhook (newsList: News[], webhook: string): Promise<void> {
+  process.env.WEBHOOK_URL = process.env.WEBHOOK_URL || webhook
+
   for (const news of newsList) {
     if (await wasSentBefore(news) == false) {
-      const token = process.env.WEBHOOK_URL || webhook
-
       const webhookClient = new WebhookClient({
-        url: token
+        url: process.env.WEBHOOK_URL,
       })
   
       const embed = new MessageEmbed()
