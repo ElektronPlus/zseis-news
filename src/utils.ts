217d3fb53abaf8e6b12a48ca8898e84bb47029ff
konhi <hello.konhi@gmail.com>
2021-09-18 14:24:05 +0200

    feat: get desired element content util

--- src/utils.ts
+++ src/utils.ts
@@ -3,3 +3,13 @@ import fs from 'fs'
 export async function writeObjectToFile (path: string, obj: object): Promise<void> {
   fs.writeFileSync(path, JSON.stringify(obj, null, 2), 'utf-8')
 }
+
+/**
+ * @param  {Element} element
+ * @return image `src` or text `textContent`
+ */
+export async function getDesiredElementContent (element: Element): Promise<string> {
+  // element.src throws an error because of external library, can be ignored
+  // @ts-expect-error
+  return element.src ?? element.textContent
+}
